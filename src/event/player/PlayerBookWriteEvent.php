ffd752446c5765a6a141ec61e3bca9eb8597e73f
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-29 18:46:37 +1000

    Create PlayerBookWriteEvent.php
    More to come

---
+++ src/event/player/PlayerBookWriteEvent.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PocketmineMP;
+use pocketmine\event\Listener;
+use pocketmine\item\WritableBookBase;
+use NBT;
+
+class PlayerBookWriteEvent implements Listener {
+public function onWrite(Listener $event , NBT $nbt) {
+return onWrite;
+}
+}
